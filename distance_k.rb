2c1ebbdd9026b88bc5b17b5af69ee271f987eaab
Eric Lopez <e_lo28@yahoo.com>
2019-09-02 21:05:02 -0700

    new problem added

---
+++ distance_k.rb
@@ -0,0 +1,5 @@
+# We are given a binary tree (with root node 'root'), a target node, 
+# and an integer value k.
+
+#	Return a list of the values of all nodes that have a distance k 
+# from the target node. The answer can be returned in any order.
