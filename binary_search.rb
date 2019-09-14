d7b2ff506636e6e2604ad2dee1cef29bc5e4f521
Eric Lopez <e_lo28@yahoo.com>
2019-09-13 20:48:43 -0700

    completed solution for binary search in a rotated array

--- binary_search.rb
+++ binary_search.rb
@@ -1,15 +1,48 @@
+# Suppose an array sorted in acsending order is rotated at some pivot
+# unknown to you before hand.
+# (ie. [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2])
+
+# You are given a target value to search. If found in the array return
+# its index, otherwise return -1.
+# You may assume no duplicates exists in the array.
+
+# Runtime complexity O(log n)
+# Space complexity O(1)
+
 def search (nums, target)
   start, last = 0, (nums.length - 1)
 
   while start <= last
-    mid_i = start + ((last - start) / 2)
-    if nums[mid_i] == target
-      
-    elsif nums[mid_i] > target
-      last = mid_i - 1
+    mid = start + ((last - start + 1) >> 1)
+
+    if nums[mid] == target
+      return mid
+    elsif nums[start] > nums[last]
+      if nums[mid] > nums[last]
+        if target < nums[mid] && target >= nums[start]
+          last = mid - 1
+        else
+          start = mid + 1
+        end
+      else
+        if target > nums[mid] && target <= nums[last]
+          start = mid + 1
+        else
+          last = mid - 1
+        end
+      end
+    elsif nums[mid] > target
+      last = mid - 1
     else
-      start = mid_i + 1
+      start = mid + 1
     end
   end
+
   -1
 end
+
+arr = [4,5,6,7,0,1,2]
+p "input array: ", arr
+
+puts "search(array, 0) returned: #{search(arr, 0)}"
+puts "search(array, 3) returned: #{search(arr, 3)}"
